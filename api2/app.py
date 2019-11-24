c5f36bda582a20f42fec809fbe71fb21d34fcfb1
Ashwani <ashwanitr001@gmail.com>
2019-11-24 18:55:55 +0530

    updated the app and security

--- api2/app.py
+++ api2/app.py
@@ -8,7 +8,7 @@ from item import Item, ItemList
 
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True
-app.secret_key = 'jose'
+app.secret_key = 'Ashwani'
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
--- api2/security.py
+++ api2/security.py
@@ -1,10 +1,9 @@
-from werkzeug.security import safe_str_cmp
 from user import User
 
 
 def authenticate(username, password):
     user = User.find_by_username(username)
-    if user and safe_str_cmp(user.password, password):
+    if user and user.password == password:
         return user
 
 
