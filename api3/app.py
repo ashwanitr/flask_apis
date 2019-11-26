b7b43958b6986a41eaf3bd4401d5c689b8902857
Ashwani <ashwanitr001@gmail.com>
2019-11-26 16:00:13 +0530

    updated the api3

--- api3/app.py
+++ api3/app.py
@@ -5,15 +5,25 @@ from flask_jwt import JWT
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['PROPAGATE_EXCEPTIONS'] = True
-app.secret_key = 'Ashwani'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.secret_key = 'jose'
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+jwt = JWT(app, authenticate, identity)  # /auth
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
@@ -21,4 +31,4 @@ api.add_resource(UserRegister, '/register')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(debug=True)  # important to mention debug=True
+    app.run(port=5000, debug=True)
--- api3/resources/item.py
+++ api3/resources/item.py
@@ -1,18 +1,20 @@
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
-import sqlite3
 
 
 class Item(Resource):
-    TABLE_NAME = 'items'
-    
     parser = reqparse.RequestParser()
     parser.add_argument('price',
-        type=float,
-        required=True,
-        help="This field cannot be left blank!"
-    )
+                        type=float,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('store_id',
+                        type=int,
+                        required=True,
+                        help="Every item needs a store_id."
+                        )
 
     @jwt_required()
     def get(self, name):
@@ -23,66 +25,41 @@ class Item(Resource):
 
     def post(self, name):
         if ItemModel.find_by_name(name):
-            return {'message': "An item with name '{}' already exists.".format(name)}
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400
 
         data = Item.parser.parse_args()
 
-        item = ItemModel(name,data['price'])
+        item = ItemModel(name, **data)
 
         try:
-            item.insert()
+            item.save_to_db()
         except:
-            return {"message": "An error occurred inserting the item."}
+            return {"message": "An error occurred inserting the item."}, 500
+
+        return item.json(), 201
+
+    def delete(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
+            return {'message': 'Item deleted.'}
+        return {'message': 'Item not found.'}, 404
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+
+        item = ItemModel.find_by_name(name)
+
+        if item:
+            item.price = data['price']
+        else:
+            item = ItemModel(name, **data)
+
+        item.save_to_db()
 
         return item.json()
 
-    @jwt_required()
-    def delete(self, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "DELETE FROM {table} WHERE name=?".format(table=self.TABLE_NAME)
-        cursor.execute(query, (name,))
-
-        connection.commit()
-        connection.close()
-
-        return {'message': 'Item deleted'}
-
-    @jwt_required()
-    def put(self, name):
-        data = Item.parser.parse_args()
-        item = ItemModel.find_by_name(name)
-        print(item.name)
-        print(item.price)
-        updated_item = ItemModel(name,data['price'])
-        print(updated_item.price)
-        if item is None:
-            try:
-                updated_item.insert()
-            except:
-                return {"message": "An error occurred inserting the item."}
-        else:
-            try:
-                updated_item.update()
-            except:
-                
-                return {"message": "An error occurred updating the item."}
-        return updated_item.json()
-
 
 class ItemList(Resource):
-    TABLE_NAME = 'items'
-
     def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM {table}".format(table=self.TABLE_NAME)
-        result = cursor.execute(query)
-        items = []
-        for row in result:
-            items.append({'name': row[0], 'price': row[1]})
-        connection.close()
-
-        return {'items': items}
+        return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))}
