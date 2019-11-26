4677bbaccfb1cbb034922409ce0608fac7cb7be3
Ashwani <ashwanitr001@gmail.com>
2019-11-26 16:35:28 +0530

    updated api3

--- api3/security.py
+++ api3/security.py
@@ -1,10 +1,9 @@
-from werkzeug.security import safe_str_cmp
 from models.user import UserModel
 
 
 def authenticate(username, password):
     user = UserModel.find_by_username(username)
-    if user and safe_str_cmp(user.password, password):
+    if user and user.password == password:
         return user
 
 
